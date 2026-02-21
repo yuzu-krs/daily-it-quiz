b4ba94b5345b9196f5d01ba8e77cfc5b01a3798b
yuzu-krs <yuzuto.poi@gmail.com>
2026-02-21 09:41:15 +0900

    fix:朝に3問でる

--- bot.py
+++ bot.py
@@ -149,9 +149,9 @@ async def scheduled_quiz():
     
     # 7:00 に実行
     target_times = [
-        time(7, 0),   # 朝7時
-        time(7, 1),   # 朝7時
-        time(7, 2),   # 朝7時
+        time(7, 0, 0),   # 朝7時
+        time(7, 0, 1),   # 朝7時0分1秒
+        time(7, 0, 2),   # 朝7時0分2秒
     ]
     
     # 1分以内の誤差を許容
